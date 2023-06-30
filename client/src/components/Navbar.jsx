6b8a2c0ec6cb1971453c163aff5c67e10ee24e02
mateipruteanu <mateipruteanu1@gmail.com>
2023-06-30 20:32:24 +0300

    implemented a responsive navbar
    on mobile it becomes a hamburger menu

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,64 @@
-// import React from "react";
-import NavbarButtons from "./NavbarButtons.jsx";
-import Name from "./Name.jsx";
+import {NavLink, useLocation} from "react-router-dom";
+import Button from "./Button.jsx";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars, faXmark} from "@fortawesome/free-solid-svg-icons";
+import React, {useEffect, useState} from "react";
+import "../styles/Navbar.css";
 
 function Navbar() {
+    const [showNavbar, setShowNavbar] = useState(false)
+    const location = useLocation();
+
+
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar);
+    }
+
+    function setColor(newColor) {
+        document.documentElement.style.setProperty('--nav-text-color', newColor);
+    }
+
+    useEffect(() => {
+        if (location.pathname === "/") {
+            setColor("#ffffff") // white
+        } else {
+            setColor("#000000"); // black
+        }
+
+        // for mobile navbar
+        if (window.innerWidth < 672) {
+            setColor("#ffffff") // white
+        }
+
+    }, [location, window]);
+
+
     return (
-        <div className={"navbar"}>
-            <Name/>
-            <NavbarButtons/>
-        </div>
+        <nav className={"navbar"}>
+            <div className={"container"}>
+                <h1 className={"name"}> mateipruteanu_ </h1>
+                <div className={"menu-icon"} onClick={handleShowNavbar}>
+                    <FontAwesomeIcon icon={showNavbar ? faXmark : faBars}/>
+                </div>
+                <div className={`nav-elements ${showNavbar && 'active'}`}>
+                    <ul>
+                        <li>
+                            <NavLink to={"/"}> Home </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={"/projects"}> Projects </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={"/about"}> About </NavLink>
+                        </li>
+                        <li>
+                            <Button buttonLink={"mailto: mateipruteanu1@gmail.com"} className={"blue-button"}
+                                    buttonText={"GET IN TOUCH"}/>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     );
 }
 
