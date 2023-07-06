9efb30ce6ae1b0c9b0e23e1463e7632e6b19663d
mateipruteanu <mateipruteanu1@gmail.com>
2023-07-06 20:28:55 +0300

    now closing navbar after click on navlink

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,34 +6,38 @@ import React, {useEffect, useState} from "react";
 import "../styles/Navbar.css";
 
 function Navbar() {
-    const [showNavbar, setShowNavbar] = useState(false)
+    const [showNavbar, setShowNavbar] = useState(false);
     const location = useLocation();
-
 
     const handleShowNavbar = () => {
         setShowNavbar(!showNavbar);
-    }
+    };
+
+    const handleNavLinkClick = () => {
+        setShowNavbar(false);
+    };
 
     function setColor(newColor) {
-        document.documentElement.style.setProperty('--nav-text-color', newColor);
+        document.documentElement.style.setProperty("--nav-text-color", newColor);
     }
 
     useEffect(() => {
         if (location.pathname === "/") {
-            setColor("#ffffff") // white
+            setColor("#ffffff"); // white
         } else {
             setColor("#000000"); // black
         }
 
         // for mobile navbar
         if (window.innerWidth < 672) {
-            setColor("#000000") // black
+            setColor("#000000"); // black
             document.querySelector(".blue-button").classList.add("main-gradient-button");
             document.querySelector(".blue-button").style.color = "#ffffff";
+        } else {
+            document.querySelector(".blue-button").classList.remove("main-gradient-button");
+            document.querySelector(".blue-button").style.color = "#000000";
         }
-
     }, [location, window]);
-
 
     return (
         <nav className={"navbar"}>
@@ -42,20 +46,30 @@ function Navbar() {
                 <div className={"menu-icon"} onClick={handleShowNavbar}>
                     <FontAwesomeIcon icon={showNavbar ? faXmark : faBars}/>
                 </div>
-                <div className={`nav-elements ${showNavbar && 'active'}`}>
+                <div className={`nav-elements ${showNavbar && "active"}`}>
                     <ul>
                         <li>
-                            <NavLink to={"/"}> Home </NavLink>
+                            <NavLink to={"/"} onClick={handleNavLinkClick}>
+                                Home
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink to={"/projects"}> Projects </NavLink>
+                            <NavLink to={"/projects"} onClick={handleNavLinkClick}>
+                                Projects
+                            </NavLink>
                         </li>
                         <li>
-                            <NavLink to={"/about"}> About </NavLink>
+                            <NavLink to={"/about"} onClick={handleNavLinkClick}>
+                                About
+                            </NavLink>
                         </li>
                         <li>
-                            <Button buttonLink={"mailto: mateipruteanu1@gmail.com"} className={"blue-button"}
-                                    buttonText={"GET IN TOUCH"}/>
+                            <Button
+                                buttonLink={"mailto: mateipruteanu1@gmail.com"}
+                                className={"blue-button"}
+                                buttonText={"GET IN TOUCH"}
+                                onClick={handleNavLinkClick}
+                            />
                         </li>
                     </ul>
                 </div>
