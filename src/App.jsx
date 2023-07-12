d56a240b62603239f3df611d97e2eee3c73c1d0c
mateipruteanu <mateipruteanu1@gmail.com>
2023-07-12 22:39:04 +0300

    added loading screen (not used yet)

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,56 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import './App.css'
-import Navbar from "./components/Navbar.jsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./components/HomePage/Home.jsx";
-import Projects from "./components/ProjectsPage/Projects.jsx";
-import About from "./components/AboutPage/About.jsx";
-import * as insights from "insights-js";
-import {parameters} from "insights-js";
+import React, {useEffect, useState} from 'react';
+import {Route, Routes} from 'react-router-dom';
+import Home from './components/HomePage/Home.jsx';
+import Projects from './components/ProjectsPage/Projects.jsx';
+import About from './components/AboutPage/About.jsx';
+import './styles/App.css';
+import * as insights from 'insights-js';
+import {parameters} from 'insights-js';
+import Loading from "./components/Loading.jsx";
 
 // tracking page views
 insights.init('SIbKVeVXPwRxphQW');
 insights.trackPages();
 insights.track({
-    id: "open-page",
+    id: 'open-page',
     parameters: {
         path: parameters.path(),
         screenType: parameters.screenType(),
-        referrer: parameters.referrer()
-    }
+        referrer: parameters.referrer(),
+    },
 });
 
 function App() {
+    // const [isContentLoaded, setIsContentLoaded] = useState(false);
+
+    // useEffect(() => {
+    //     setIsContentLoaded(false);
+    // }, []);
+
+    // const handleContentLoad = () => {
+    //     setIsContentLoaded(true);
+    // }
+
     return (
-        <BrowserRouter>
-            <div>
-                <Navbar/>
-                <Routes>
-                    <Route exact path={"/"} element={<Home/>}/>
-                    <Route exact path={"/projects"} element={<Projects/>}/>
-                    <Route exact path={"/about"} element={<About/>}/>
-                </Routes>
-            </div>
-        </BrowserRouter>
+        <div>
+            <Routes>
+                <Route
+                    exact
+                    path={'/'}
+                    element={<Home/>}
+                />
+                <Route
+                    exact
+                    path={'/projects'}
+                    element={<Projects/>}
+                />
+                <Route
+                    exact
+                    path={'/about'}
+                    element={<About/>}
+                />
+            </Routes>
+        </div>
     );
 }
 
